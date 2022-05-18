c1c766a573006821aa45da15dca56fae1e88ffa6
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-05-19 03:20:48 +0700

    Create hbs template and Add tailwind css cdn

--- tanny_board/index.js
+++ tanny_board/index.js
@@ -1,15 +1,16 @@
 const express = require("express"); //import express
 const app = express(); //สร้างobject app
 app.use(express.urlencoded({ extended: true })); //เพื่อให้รับค่าข้อมูลเเบบฟอร์มจากpost manได้
+app.set("view engine", "hbs");//สร้างtemplate htmlที่ไม่ใช่ไฟล์ html (handlebars)
 //request = ข้อมูลที่userส่งมาให้เรา, response = สิ่งที่เราต้องการเเสดงผลตอบกลับuser
 //get ใช้อ่านข้อมูล postใช้เพิ่ม อัปเดต ลบข้อมูล
 app.get("/", (request, response) => {
   const { q, sortBy } = request.query; //รับข้อมูลเข้าทางurl ต่อท้ายด้วย?q='pudding'&sortBy='sth'
-  response.send("Hello bitch!, I want to eat " + q); //เเสดงผลให้userเห็น
+  response.render('home', {q,sortBy}) //เเสดงผลให้userเห็น
 });
 
 app.get("/p/new", (request, response) => {
-  response.send("Add new post");
+  response.render('postNew')
 });
 
 app.post("/p/new", (request, response) => {
@@ -22,7 +23,7 @@ app.post("/p/new", (request, response) => {
 
 app.get("/p/:postId", (request, response) => {
   const { postId } = request.params; //ใส่ข้อมูลเป็นเลขหลังurl
-  response.send(`Hello bicth again!, this post is id = ${postId} `);
+  response.render('post', {postId})
 });
 
 app.listen(9753, () => {
